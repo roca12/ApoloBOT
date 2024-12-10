091481b5de31afbff82faee6d04b4070d51c17cd
anfeespi <andres.espitia.afer@gmail.com>
2024-12-10 16:12:02 -0500

    Added 2 functionalities, first of them is the training crud only with create, and the response of the "teamo" command

--- src/main/java/com/roca12/apolobot/controller/AplMain.java
+++ src/main/java/com/roca12/apolobot/controller/AplMain.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
+import com.roca12.apolobot.util.ILoveResponses;
 import org.javacord.api.DiscordApi;
 import org.javacord.api.DiscordApiBuilder;
 import org.springframework.core.io.ClassPathResource;
@@ -17,108 +18,107 @@ import com.roca12.apolobot.service.ReRunApoloService;
 public class AplMain {
 
 
-	private final boolean PRODUCTION_STATE = false;
+    private final boolean PRODUCTION_STATE = false;
 
 
-	private String token;
-	private DiscordApi api;
+    private String token;
+    private DiscordApi api;
 
-	private SlashBuilder sb;
-	private SlashListener sl;
-	private MessageListener ml;
-	private LessonMessageSender ms;
+    private SlashBuilder sb;
+    private SlashListener sl;
+    private MessageListener ml;
+    private LessonMessageSender ms;
 
-	private Properties prop = new Properties();
-	
-	//private GroceryItemDAO giDao;
-	private ReRunApoloService rraDao;
+    private Properties prop = new Properties();
 
-	public AplMain() {
-		//giDao = new GroceryItemDAO();
-		rraDao = new ReRunApoloService();
-	}
+    //private GroceryItemDAO giDao;
+    private ReRunApoloService rraDao;
 
-	public void run() {
-		
-		loadAndCheckCriticals();
-		System.out.println("Starting Apolo bot");
-		try {
-			if (checkReqs()) {
+    public AplMain() {
+        //giDao = new GroceryItemDAO();
+        rraDao = new ReRunApoloService();
+    }
 
-				System.out.println("Invite the bot with the following url: " + api.createBotInvite());
+    public void run() {
 
-				api.bulkOverwriteGlobalApplicationCommands(sb.initSlashCommands()).join();
+        loadAndCheckCriticals();
+        System.out.println("Starting Apolo bot");
+        try {
+            if (checkReqs()) {
 
-				printInfo();
+                System.out.println("Invite the bot with the following url: " + api.createBotInvite());
 
-				api.addListener(ml);
+                api.bulkOverwriteGlobalApplicationCommands(sb.initSlashCommands()).join();
 
-				sl.handleSlashComms();
+                printInfo();
 
-				ms.setTimer();
+                api.addListener(ml);
 
-			} else {
-				System.out.println("something is broken, check reqs");
-				System.exit(0);
-			}
+                sl.handleSlashComms();
 
-		} catch (Exception e) {
-			e.printStackTrace();
-			System.exit(0);
-		}
+                ms.setTimer();
 
-	}
+            } else {
+                System.out.println("something is broken, check reqs");
+                System.exit(0);
+            }
 
-	private boolean loadAndCheckCriticals() {
-		//System.out.println(System.getProperty("user.dir"));
-		try {
-			InputStream r = new ClassPathResource("files/main.properties").getInputStream();
-			prop.load(r);
-			//System.out.println("token " + prop.getProperty("apolo.test.token"));
-			if (PRODUCTION_STATE) {
-				System.out.println("Running in prod mode");
-				token = prop.getProperty("apolo.prod.token");
-			} else {
-				System.out.println("Running in dev test mode");
-				token = prop.getProperty("apolo.test.token");
-			}
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(0);
+        }
 
-			api = new DiscordApiBuilder().setToken(token).setAllIntents().login().join();
-			sb = new SlashBuilder(api);
-			sl = new SlashListener(api);
-			ml = new MessageListener(api);
-			ms = new LessonMessageSender(api, PRODUCTION_STATE);
-			
-			testMongoDB(); 
+    }
 
-			return true;
+    private boolean loadAndCheckCriticals() {
+        //System.out.println(System.getProperty("user.dir"));
+        try {
+            InputStream r = new ClassPathResource("files/main.properties").getInputStream();
+            prop.load(r);
+            //System.out.println("token " + prop.getProperty("apolo.test.token"));
+            if (PRODUCTION_STATE) {
+                System.out.println("Running in prod mode");
+                token = prop.getProperty("apolo.prod.token");
+            } else {
+                System.out.println("Running in dev test mode");
+                token = prop.getProperty("apolo.test.token");
+            }
 
-		} catch (IOException e) {
-			e.printStackTrace();
-			System.err.println("main.properties error");
-			return false;
-		}
-		
-		
+            api = new DiscordApiBuilder().setToken(token).setAllIntents().login().join();
+            sb = new SlashBuilder(api);
+            sl = new SlashListener(api);
+            ml = new MessageListener(api);
+            ms = new LessonMessageSender(api, PRODUCTION_STATE);
 
-	}
+            testMongoDB();
 
-	private void printInfo() {
-		System.out.println("Client:" + api.getClientId());
-	}
+            return true;
 
-	private boolean checkReqs() {
-		if (token.isBlank()) {
-			System.err.println("Discord token error");
-			return false;
-		} else {
-			return true;
-		}
-	}
-	
-	private void testMongoDB() {
-		rraDao.createNewReRun();
-	}
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.err.println("main.properties error");
+            return false;
+        }
+
+
+    }
+
+    private void printInfo() {
+        System.out.println("Client:" + api.getClientId());
+    }
+
+    private boolean checkReqs() {
+        if (token.isBlank()) {
+            System.err.println("Discord token error");
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    private void testMongoDB() {
+        rraDao.createNewReRun();
+    }
 
 //	private void testMongoDB() {
 //
@@ -148,7 +148,6 @@ public class AplMain {
 //		System.out.println("\n-------------------THANK YOU---------------------------");
 //
 //	}
-	
-	
+
 
 }
--- src/main/java/com/roca12/apolobot/controller/handler/SlashBuilder.java
+++ src/main/java/com/roca12/apolobot/controller/handler/SlashBuilder.java
@@ -1,85 +1,103 @@
 package com.roca12.apolobot.controller.handler;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Properties;
-import java.util.Set;
+import java.util.*;
 
 import org.javacord.api.DiscordApi;
-import org.javacord.api.interaction.SlashCommand;
-import org.javacord.api.interaction.SlashCommandBuilder;
-import org.javacord.api.interaction.SlashCommandOption;
-import org.javacord.api.interaction.SlashCommandOptionType;
+import org.javacord.api.interaction.*;
 
 public class SlashBuilder {
-	
-	private DiscordApi api;
-	
-	private Set<SlashCommandBuilder> builders;
-	
-	private Properties prop = new Properties();
 
-	public SlashBuilder(DiscordApi api) {
-		this.api=api;
-		builders = new HashSet<>();
-	}
+    private DiscordApi api;
 
-	public Set<SlashCommandBuilder> initSlashCommands() {
+    private Set<SlashCommandBuilder> builders;
 
-		builders.add(new SlashCommandBuilder().setName("ayuda")
-				.setDescription("Muestra la lista completa de comandos y quienes pueden usarlo"));
+    private Properties prop = new Properties();
 
-		builders.add(new SlashCommandBuilder().setName("ping").setDescription("Retorna un saludo conocido"));
+    public SlashBuilder(DiscordApi api) {
+        this.api = api;
+        builders = new HashSet<>();
+    }
 
-		builders.add(new SlashCommandBuilder().setName("test")
-				.setDescription("Verifica la conectividad de todos los sistemas"));
+    public Set<SlashCommandBuilder> initSlashCommands() {
 
-		builders.add(new SlashCommandBuilder().setName("numerousuarios")
-				.setDescription("Cuenta la cantidad de usuarios existentes en el servidor"));
+        builders.add(new SlashCommandBuilder().setName("ayuda")
+                .setDescription("Muestra la lista completa de comandos y quienes pueden usarlo"));
 
-		builders.add(SlashCommand.with("entrenamiento", "Capacidad de anunciar o cancelar clases",
-				Arrays.asList(
-						SlashCommandOption.createWithOptions(SlashCommandOptionType.SUB_COMMAND, "Cancelar",
-								"Cancela la siguiente clase"),
-						SlashCommandOption.createWithOptions(SlashCommandOptionType.SUB_COMMAND, "Anunciar",
-								"Envia un aviso a todos los participantes de este canal de texto"))));
+        builders.add(new SlashCommandBuilder().setName("ping").setDescription("Retorna un saludo conocido"));
 
-		builders.add(SlashCommand.with("evento",
-				"Administra eventos futuros, tendra un recordatorio un dia antes y una hora antes",
-				Arrays.asList(
-						SlashCommandOption.createWithOptions(SlashCommandOptionType.SUB_COMMAND, "Agregar",
-								"Agrega nuevo evento"),
-						SlashCommandOption.createWithOptions(SlashCommandOptionType.SUB_COMMAND, "Eliminar",
-								"Elimina un evento por ID"),
-						SlashCommandOption.createWithOptions(SlashCommandOptionType.SUB_COMMAND, "Actualizar",
-								"Actualiza un evento por ID"),
-						SlashCommandOption.createWithOptions(SlashCommandOptionType.SUB_COMMAND, "Listar",
-								"Lista todos los eventos"))));
+        builders.add(new SlashCommandBuilder().setName("test")
+                .setDescription("Verifica la conectividad de todos los sistemas"));
 
-		return builders;
-	}
+        builders.add(new SlashCommandBuilder().setName("numerousuarios")
+                .setDescription("Cuenta la cantidad de usuarios existentes en el servidor"));
 
-	public Set<SlashCommandBuilder> getBuilders() {
-		return builders;
-	}
+        builders.add(
+                new SlashCommandBuilder()
+                        .setName("entrenamiento")
+                        .setDescription("Capacidad de anunciar o cancelar clases")
+                        .addOption(
+                                new SlashCommandOptionBuilder()
+                                        .setName("cancelar")
+                                        .setDescription("Cancela la clase en la fecha especificada")
+                                        .setType(SlashCommandOptionType.SUB_COMMAND)
+                                        .addOption(
+                                                SlashCommandOption.createStringOption("fecha", "La fecha de la clase en formato dd-MM-yyyy HH:mm", true)
+                                        )
+                                        .build()
+                        )
+                        .addOption(
+                                new SlashCommandOptionBuilder()
+                                        .setName("anunciar")
+                                        .setDescription("Envía un viso a todos los participantes acerca de una fecha de entrenamiento")
+                                        .setType(SlashCommandOptionType.SUB_COMMAND)
+                                        .addOption(
+                                                SlashCommandOption.createStringOption("fecha", "La fecha de la clase en formato dd-MM-yyyy HH:mm", true)
+                                        )
+                                        .build()
+                        )
+        );
 
-	public void setBuilders(Set<SlashCommandBuilder> builders) {
-		this.builders = builders;
-	}
+        builders.add(
+                new SlashCommandBuilder().setName("teamo")
+                        .setDescription("Demuestra tu amor por el bot :D")
+        );
 
-	public Properties getProp() {
-		return prop;
-	}
+        builders.add(SlashCommand.with("evento",
+                "Administra eventos futuros, tendra un recordatorio un dia antes y una hora antes",
+                Arrays.asList(
+                        SlashCommandOption.createWithOptions(SlashCommandOptionType.SUB_COMMAND, "Agregar",
+                                "Agrega nuevo evento"),
+                        SlashCommandOption.createWithOptions(SlashCommandOptionType.SUB_COMMAND, "Eliminar",
+                                "Elimina un evento por ID"),
+                        SlashCommandOption.createWithOptions(SlashCommandOptionType.SUB_COMMAND, "Actualizar",
+                                "Actualiza un evento por ID"),
+                        SlashCommandOption.createWithOptions(SlashCommandOptionType.SUB_COMMAND, "Listar",
+                                "Lista todos los eventos"))));
 
-	public void setProp(Properties prop) {
-		this.prop = prop;
-	}
+        return builders;
+    }
 
-	public DiscordApi getApi() {
-		return api;
-	}
+    public Set<SlashCommandBuilder> getBuilders() {
+        return builders;
+    }
 
-	public void setApi(DiscordApi api) {
-		this.api = api;
-	}
+    public void setBuilders(Set<SlashCommandBuilder> builders) {
+        this.builders = builders;
+    }
+
+    public Properties getProp() {
+        return prop;
+    }
+
+    public void setProp(Properties prop) {
+        this.prop = prop;
+    }
+
+    public DiscordApi getApi() {
+        return api;
+    }
+
+    public void setApi(DiscordApi api) {
+        this.api = api;
+    }
 }
